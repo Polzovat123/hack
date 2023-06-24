97242240caa9649b26960b456d15f5fa5b755618
useri <useri@LAPTOP-2IUQ8AUK>
2023-06-24 11:29:26 +0500

    add Config file

--- Application/pdan.py
+++ Application/pdan.py
@@ -20,3 +20,13 @@ class Files(BaseModel):
 class ResponsePDF(BaseModel):
     id: int
     files: List[Files]
+
+
+class ConfigurationIMGParser(BaseModel):
+    name_model: Optional[str]
+    level_using: int #[0-not uses, 1-use only for img page, 2-use always]
+
+
+class Configuration(BaseModel):
+    config_model: ConfigurationIMGParser = ConfigurationIMGParser(level_using=0)
+    level_detection: int = 0
