750afb60702ca79b9c15b21ad42bd784347f6247
useri <useri@LAPTOP-2IUQ8AUK>
2023-06-25 01:12:39 +0500

    fix

--- Application/pipeline.py
+++ Application/pipeline.py
@@ -7,15 +7,21 @@ from Application.ml_models.name_detection.level_2.fasttext_model import FastText
 from Application.ml_models.name_detection.level_3.sbert import SBERTModel
 from Application.ml_models.name_recognition.level_3.stanza import StanzaNameRecognition
 from Application.ml_models.validation.level_1.heuristic import find_string_differences
+from Application.pdan import Files
 
 
 def process_file(file_pdf, id, extra_name, string_from_pdf, level_naming_detection=1, session=False, config=None):
-    files = []
-    print()
     if config is None:
         if level_naming_detection == 1:
             print('heuristic')
-            files = HeuristicModel(find_string_differences).execute(file_pdf, id, extra_name, string_from_pdf)
+            # files = HeuristicModel(find_string_differences).execute(file_pdf, id, extra_name, string_from_pdf)
+            files = [(Files(
+                file_name='1.pdf',
+                folder='12',
+                name=f'Start on {34}',
+                description='Incorrect input',
+                page=1
+            ))]
         elif level_naming_detection == 2:
             files = FastTextModel(find_string_differences).execute(file_pdf, id, extra_name, string_from_pdf)
         elif level_naming_detection == 3:
