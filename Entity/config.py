165cd92ac18beb99e9b3f1d93d3d8209e43dd28a
useri <useri@LAPTOP-2IUQ8AUK>
2023-06-24 12:04:40 +0500

    standard configuration entity

---
+++ Entity/config.py
@@ -0,0 +1,17 @@
+
+class ConfigValidator:
+    def __init__(self, name_model):
+            self.name_model = name_model
+
+
+class ConfigIMG:
+    def __init__(self, name_model, level_using):
+        self.name_model = name_model
+        self.level_using = level_using
+
+
+class ConfigCompute:
+    def __init__(self, level_detection, name_img_parser, level_using, name_validator):
+        self.level_detection = level_detection
+        self.config_img_parser = ConfigIMG(name_img_parser, level_using)
+        self.config_validator = name_validator
