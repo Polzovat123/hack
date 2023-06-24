8123e4c83d1a348cd623173118f3f56bfdcb3ada
useri <useri@LAPTOP-2IUQ8AUK>
2023-06-24 09:05:09 +0500

    integrate validator model in base pipeline models

--- Application/ml_models/name_detection/level_1/heuristic.py
+++ Application/ml_models/name_detection/level_1/heuristic.py
@@ -22,42 +22,58 @@ class HeuristicModel(ExecuteModel):
 
         return np.array(minima)
 
-    def _fuzzy_find(self, text: str, value: str, max_dist: int = 10) -> list[int]:
+    def _fuzzy_find(self, text: str, value: str, max_dist: int = 10):
         len_str = len(value)
 
         text_std = self._standardize(text)
         value_std = self._standardize(value)
 
         distances = []
+        reason = []
+
         for i in range(1, len(text_std) - len(value_std)):
             distances.append(lev.distance(text_std[i: i + len(value_std)], value_std))
 
+            allowed, not_allowed = find_string_differences(
+                value_std, text_std[i: i + len(value_std)]
+            )
+            rs = "Allowed differences:\n"
+            for diff in allowed:
+                rs = rs + f"  - {diff}\n"
+
+            rs = rs + "\nUnallowed differences:\n"
+            for diff in not_allowed:
+                rs = rs + f"  - {diff}\n"
+            reason.append(rs)
+
         distances = np.array(distances)
         minima_indecies = self._find_local_minima_with_plateau(distances)
-        if (len(minima_indecies) == 0): return []
+        if (len(minima_indecies) == 0): return [], []
         filter = distances[minima_indecies] < max_dist
 
         ans = []
+        rs = []
         last = -1
-        for elem_pos in minima_indecies[filter]:
+        for ind, elem_pos in enumerate(minima_indecies[filter]):
             if last != -1 and last + len_str > elem_pos:
                 pass
             else:
                 ans.append(elem_pos)
+                rs.append(reason[ind])
                 last = elem_pos
-        return ans
+        return ans, rs
 
     def execute(self, file_name, folder, correct_name, page_text):
         ans = []
 
         for page_num, page in enumerate(page_text):
-            match_starts = self._fuzzy_find('перед началом текста '+ page+ ' концовка текста', correct_name)
+            match_starts, rs = self._fuzzy_find(page, correct_name)
             for elem_add in match_starts:
                 ans.append(Files(
                     file_name=file_name,
-                    folder=folder,
+                    folder=str(folder),
                     name=f'Start on {elem_add}',
-                    description='rs',
+                    description=rs[0],
                     page=page_num
                 ))
             if (len(match_starts) != 0):
